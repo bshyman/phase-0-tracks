2e3f12aff39b59793e1643e95882e2fe57905ffa
Benjamin Shyman <bshyman@gmail.com>
2016-03-25 11:08:39 -0500

    Added content to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+puts "Hi new employee!"
+puts "What's your name?"
+name = gets.chomp
+puts "Thanks #{name}, Welcome!"
+puts "How old are you?"
+age = gets.chomp
+puts "Great. So you were born in...?"
+birth_year = gets.chomp
+puts "Right. Would you like some garlic bread?"
+garlic_bread = gets.chomp
+puts "No problem. Would you like health insurance?"
+health_ins = gets.chomp
+puts "Got it, thanks!"
