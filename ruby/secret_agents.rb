fbed6ded6e5382102b9a3b7e924168c3fc32b859
Benjamin Shyman <bshyman@gmail.com>
2016-03-26 15:02:15 -0500

    Secret agent file release 4,5

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,3 +1,26 @@
+puts "Secret agent, would you like to encrypt or decrypt your password?"
+option = gets.chomp
+puts "Enter password please"
+password = gets.chomp
+
+def encrypt (orig_string)
+  orig_string.downcase!
+  i = 0
+  new_string = ""
+  
+  until i==orig_string.length
+    if orig_string[i] == "z"
+      new_string += "a"
+    elsif orig_string[i] == " "
+      new_string += " "
+    else
+      new_string += orig_string[i].next
+    end
+    i += 1
+  end
+  new_string
+end
+
 def prev_letter (orig_letter)
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   i = alphabet.index(orig_letter) - 1
@@ -5,39 +28,23 @@ def prev_letter (orig_letter)
 end
 
 def decrypt (string1)
+  string1.downcase!
   i = 0
   new_string2 = ""
   until i==string1.length
-    new_string2 += prev_letter(string1[i])
-    i += 1
-  end
-  puts new_string2
-end
-  
-
-
-
-
-
-def encrypt (orig_string)
-  i = 0
-  new_string = ""
-  
-  until i==orig_string.length
-    if orig_string[i] == "z"
-      new_string += "a"
+    if string1[i] == " "
+      new_string2 += " "
     else
-      new_string += orig_string[i].next
+      new_string2 += prev_letter(string1[i])
     end
     i += 1
   end
-  puts new_string
-
+  new_string2
 end
-
-
-encrypt ("afe")
-encrypt ("zed")
-decrypt ("bcd")
-
-decrypt ("afe")
+  
+  if option =="encrypt"
+   puts encrypt (password)
+  else option == "decrypt"
+    puts decrypt (password)
+  end
+abort("Your data is secure")
