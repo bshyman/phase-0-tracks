7c100101b6658fa57f927c8b0a7368c34518f49c
Benjamin Shyman <bshyman@gmail.com>
2016-03-27 12:49:39 -0500

    Finished 4.4 vampires loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,12 +1,20 @@
-#object is to determine if a user is a vampitre based on input criteria
+#object is to determine if a user is a vampire based on input criteria
+
+puts "How many users will be processed?"
+num_users = gets.chomp.to_i
+num_users.times do
+
 puts "Whats is your name?"
 name_var = gets.chomp
+
 puts "How old are you?"
 age = gets.chomp
 age = age.to_i
+
 puts "What year were you born?"
 birth_year = gets.chomp
 birth_year = birth_year.to_i
+current_year = Time.new.year
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 g_bread = gets.chomp
@@ -14,87 +22,43 @@ g_bread = gets.chomp
 puts "Would you like health insurance?"
 ins_var = gets.chomp
 
-current_year = Time.new.year
-current_year = current_year.to_i
-
-case  
-  when current_year-age==birth_year && (g_bread == "yes" || ins_var == "yes")
-  puts "Prob not a vamp"
-  when current_year-age!=birth_year && (g_bread == "no") || (ins_var == "no")
-  puts "Probably a vamp"
-  when (current_year-age!=birth_year) && (g_bread == "no") && (ins_var == "no")
-  puts "Almost def a vamp!"
-  when (current_year-age==birth_year && (g_bread == "yes" || ins_var == "yes")) &&  (name_var == ("Drake Cula" || "Tu Fang")) 
-  puts "Def a vamp!!!"
-  
-  else
-  	puts "Results inconclusive"
-end
-
-
-
+puts "Do you have any allergies? Please list an allergy and press enter. If you have listed all your allergies, please type done and press enter"
+allergy = gets.chomp
 
  
+	loop do 
+		puts "please list your next allergy or type done"
+		allergy = gets.chomp
+	 		if allergy == "sunshine" 
+	 		 	break
+	 		elsif allergy == "done"
+	 			puts "thank you"
+	 			break
+	 		end
+  	end
 
+	if allergy != "sunshine"
+		case  
+    		when current_year-age==birth_year && g_bread == "yes" && ins_var == "yes" && name_var == ("Drake Cula" || "TuFang")
+  				puts "You are def a vamp!!!"
+  			when current_year-age==birth_year && ((g_bread == "yes") || (ins_var == "yes"))
+    			puts "You are prob not a vamp"
+  			when (current_year-age!=birth_year) && (g_bread == "no") && (ins_var == "no")
+    			puts "You are almost def a vamp!"
+  			when current_year-age!=birth_year &&((g_bread == "no") || (ins_var == "no"))
+    			puts "You are probably a vamp"
+  			else
+    			puts "Results inconclusive"
+    		end
+	else
+		puts "Vammmmmp"
+	end
 
-
-=begin
-if (current_year - age =birth_year) && g_bread == "yes" 
-  puts "Prob not a vamp"
-else
-  puts "Vamp!"
-end
-  
-puts current_year
-=end
-=begin
-wolves_like_sunshine = true
-wolves_like_garlic = true
-vampires_like_sunshine = false
-vampires_like_garlic = false
-wolves_like_garlic && wolves_like_sunshine
-wolves_like_garlic || vampires_like_garlic
-wolves_like_garlic && vampires_like_garlic
-wolves_like_garlic && (vampires_like_sunshine || vampires_like_garlic)
-(wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine
-vampires_like_garlic
-!vampires_like_garlic
-!(wolves_like_sunshine && wolves_like_garlic)
-=end
- 
-
-
-
-=begin
-http://stackoverflow.com/questions/21761475/ruby-case-when-vs-if-elsif
-if customerName == "Fred"
-      print "Hello Fred!"
-elsif customerName == "John"
-      print "Hello John!" 
-elsif customername == "Robert"
-      print "Hello Bob!"
+puts "You are #{name_var}, #{age} years old, born in #{birth_year}."
+puts "When asked if you were interested in garlic bread or health insurance, your answers were #{g_bread} and #{ins_var}, respectively."
 end
 
-if (current_year - age =birth_year) && g_bread == "yes" 
-  puts "Prob not a vamp"
-else
-  puts "Vamp!"
-end
-  
-puts current_year
-=end
+abort("Actually, never mind! What do these questions have to do with anything? Let's all be friends.")
 
-=begin
-wolves_like_sunshine = true
-wolves_like_garlic = true
-vampires_like_sunshine = false
-vampires_like_garlic = false
-wolves_like_garlic && wolves_like_sunshine
-wolves_like_garlic || vampires_like_garlic
-wolves_like_garlic && vampires_like_garlic
-wolves_like_garlic && (vampires_like_sunshine || vampires_like_garlic)
-(wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine
-vampires_like_garlic
-!vampires_like_garlic
-!(wolves_like_sunshine && wolves_like_garlic)
-=end
+
+
