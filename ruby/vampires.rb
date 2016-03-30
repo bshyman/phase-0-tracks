8520cdc64b7f808810b345e9e448662b49c6a8ac
Benjamin Shyman <bshyman@gmail.com>
2016-03-30 13:21:10 -0500

    Completed method detectives.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,10 @@
 #object is to determine if a user is a vampitre based on input criteria
+=begin
+puts "How many users will be processed?"
+num_users = gets.chomp.to_i
+
+
+num_users.times do
 puts "Whats is your name?"
 name_var = gets.chomp
 puts "How old are you?"
@@ -15,24 +21,45 @@ puts "Would you like health insurance?"
 ins_var = gets.chomp
 
 current_year = Time.new.year
-current_year = current_year.to_i
+=end
+puts "Do you have any allergies? Please list an allergy and press enter. If you have listed all your allergies, please type done and press enter"
 
-case  
-  when current_year-age==birth_year && (g_bread == "yes" || ins_var == "yes")
-  puts "Prob not a vamp"
-  when current_year-age!=birth_year && (g_bread == "no") || (ins_var == "no")
-  puts "Probably a vamp"
-  when (current_year-age!=birth_year) && (g_bread == "no") && (ins_var == "no")
-  puts "Almost def a vamp!"
-  when (current_year-age==birth_year && (g_bread == "yes" || ins_var == "yes")) &&  (name_var == ("Drake Cula" || "Tu Fang")) 
-  puts "Def a vamp!!!"
-  
-  else
-  	puts "Results inconclusive"
+allergy = ""
+allergy = gets.chomp
+if allergy != ("sunshine" || "done")
+  puts "please list your next allergy or type done"
+redo unless allergy == "sunshine" || "done"
+elsif
+  allergy == "sunshine"
+  puts "vamp!!"
+elsif 
+  allergy == "done"
+else
+  puts "next"
+  allergy = gets.chomp
 end
 
+=begin
+case  
+  when allergy == "sunshine"
+    puts "You are def a vamp!"
+  when current_year-age==birth_year && g_bread == "yes" && ins_var == "yes" && name_var == ("Drake Cula" || "TuFang")
+  puts "You are def a vamp!!!"
+  when current_year-age==birth_year && ((g_bread == "yes") || (ins_var == "yes"))
+    puts "You are prob not a vamp"
+  when (current_year-age!=birth_year) && (g_bread == "no") && (ins_var == "no")
+    puts "You are almost def a vamp!"
+  when current_year-age!=birth_year &&((g_bread == "no") || (ins_var == "no"))
+    puts "You are probably a vamp"
+  else
+    puts "Results inconclusive"
+end
 
+puts "You are #{name_var}, #{age} years old, born in #{birth_year}."
+puts "When asked if you were interested in garlic bread or health insurance, your answers were #{g_bread} and #{ins_var}, respectively."
 
+end
+=end
 
  
 
