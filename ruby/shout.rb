bffa73ba89e70f20caa36b945f9c054c3d21e4f5
Benjamin Shyman <bshyman@gmail.com>
2016-04-10 11:51:34 -0500

    Complete 6.5 Modules

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,34 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
-	def self.yell_happily(number)
-		number.times do
-			puts "AHHH HAPPY AHHH!!!!"
-		end
-	end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yell_happily(number)
+#     number.times do
+#       puts "AHHH HAPPY AHHH!!!!"
+#     end
+#   end
 
+# end
+
+# p Shout.yell_angrily("argghhh")
+# Shout.yell_happily(5)
+
+module Shout
+  def yell_repeatedly(words)
+    words.length.times do
+      puts "#{words}"
+    end
+  end
 end
 
-p Shout.yell_angrily("argghhh")
-Shout.yell_happily(5)
+class Parent
+  include Shout
+end
+
+class Teacher
+  include Shout
+end
+parent = Parent.new
+parent.yell_repeatedly("YOU LITTLE...!!!")
+teacher = Teacher.new
+teacher.yell_repeatedly("Quiet Please!")
