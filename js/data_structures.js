7a712eb36a5dda587d62e7fb3f43da9ad8baaf6c
Benjamin Shyman <bshyman@gmail.com>
2016-04-17 21:54:51 -0500

    Complete peer pair

---
+++ js/data_structures.js
@@ -0,0 +1,34 @@
+var colors = ["blue", "red", "green", "black"];
+var horse = ["Jimmy", "Ed", "Sanders", "Bill"];
+
+var combo = {};
+              
+for (var i = 0; i < horse.length; i++) {
+  console.log[horse]
+  combo[horse[i]] = colors[i];
+}
+console.log(combo)
+
+///https://repl.it/CA4q/2
+
+function Car(color, horsepower) {
+  this.color = color;
+  this.horsepower = horsepower;
+    if (color == "blue") {
+      console.log("Blue is my favortite color and you got " + horsepower + " horsepower");}
+    else {
+      console.log("Your car is " + color + ". You also should have gotten more than " + horsepower + " horsepower");
+      
+      };
+  
+}
+
+var newest_car = new Car("red", 300);
+var newest_car = new Car("blue", 500);
+var newest_car = new Car("green", 300);
+console.log(newest_car);
+
+
+
+
+
